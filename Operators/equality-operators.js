1a4798ebdad56768160452a731ac00eeb9a370f0
Callum Leeming <callum.leeming@zuto.com>
2019-10-25 13:39:36 +0100

    Add equality-operators.js

---
+++ Operators/equality-operators.js
@@ -0,0 +1,12 @@
+//Equality Operators
+
+let var1 = 5;
+let var2 = "5";
+
+//this condition is true because it coerce's the string to a number
+console.log("1" == 1); //output: true
+console.log("1" != 1); //output: false
+
+//this condition is false as it type checks - a string isn't equal to a number
+console.log("1" === 1); //output: false
+console.log("1" !== 1); //output: true
