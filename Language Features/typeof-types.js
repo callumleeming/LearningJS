5b058de4ea50c4106266a8031279dc7681536ab8
Callum Leeming <callum.leeming@zuto.com>
2019-10-25 12:49:11 +0100

    Add typeof-types.js

---
+++ Language Features/typeof-types.js
@@ -0,0 +1,11 @@
+//TypeOf
+
+console.log(typeof(1));
+console.log(typeof("a"));
+console.log(typeof(function() {}));
+console.log(typeof(true));
+console.log(typeof({}));
+console.log(typeof([]));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(NaN));
