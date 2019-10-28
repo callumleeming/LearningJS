6c21f5c5c6960a2beff9a297e4a25a42a9df1c2d
Callum Leeming <callum.leeming@zuto.com>
2019-10-28 09:00:12 +0000

    Add output comments to typeof-types.js

--- Language Features/typeof-types.js
+++ Language Features/typeof-types.js
@@ -1,11 +1,11 @@
 //TypeOf
 
-console.log(typeof(1));
-console.log(typeof("a"));
-console.log(typeof(function() {}));
-console.log(typeof(true));
-console.log(typeof({}));
-console.log(typeof([]));
-console.log(typeof(null));
-console.log(typeof(undefined));
-console.log(typeof(NaN));
+console.log(typeof(1)); //number
+console.log(typeof("a")); //string
+console.log(typeof(function() {})); //function
+console.log(typeof(true)); //boolean
+console.log(typeof({})); //object
+console.log(typeof([])); //object
+console.log(typeof(null)); //object
+console.log(typeof(undefined)); //undefined
+console.log(typeof(NaN)); //number
