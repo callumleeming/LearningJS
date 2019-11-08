8f4ea67efb6823c66fb471b0fbbb649ea54a2616
Callum Leeming <callum.leeming@zuto.com>
2019-11-08 13:01:34 +0000

    Update prototypes.js

--- Objects and Arrays/prototypes.js
+++ Objects and Arrays/prototypes.js
@@ -10,3 +10,11 @@ Pizza.prototype.getTopping = function() {
 
 let pizza = new Pizza("Bacon and Pepperoni");
 pizza.getTopping(); //Bacon and Pepperoni
+
+//===============================================================
+
+String.prototype.bar = function() {
+    return this.toString() + " bar";
+};
+
+console.log("foo".bar()); //foo bar
