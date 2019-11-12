ac7a897a3246f53d8cd594241d02e7ca022db97b
Callum Leeming <callum.leeming@zuto.com>
2019-11-12 16:37:29 +0000

    Update open-or-senior.js comments

--- JS Katas/open-or-senior.js
+++ JS Katas/open-or-senior.js
@@ -11,6 +11,9 @@
 // Example Input
 // [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
 
+
+// I tried doing this kata without using Map. (for performance reasons)
+
 function openOrSenior(data) {
     var result = [];
 
