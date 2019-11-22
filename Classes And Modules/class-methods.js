b743fcc48dd3f2d080f3eabb58d342b321970965
Callum Leeming <callum.leeming@zuto.com>
2019-11-22 14:35:02 +0000

    Update class-methods.js

--- Classes And Modules/class-methods.js
+++ Classes And Modules/class-methods.js
@@ -16,3 +16,26 @@ let writer = new Writer("I am using a classes constructor");
 
 console.log(writer.print()); //Your message is: I am using a classes constructor. 
 console.log(writer.printType()); //Your message is a type of: string. 
+
+//====================================================================================
+// You can also extend on the class by adding prototypes
+//====================================================================================
+
+class PrototypeWriter {
+    constructor(message) {
+        this.message = message;
+    }
+}
+
+PrototypeWriter.prototype.print = function() {
+    return `Your message is: ${this.message}.`;
+}
+
+PrototypeWriter.prototype.printType = function() {
+    return `Your message is a type of: ${typeof(this.message)}.`;
+}
+
+let prototypeWriter = new PrototypeWriter("I am using a classes constructor"); 
+
+console.log(prototypeWriter.print()); //Your message is: I am using a classes constructor. 
+console.log(prototypeWriter.printType()); //Your message is a type of: string. 
