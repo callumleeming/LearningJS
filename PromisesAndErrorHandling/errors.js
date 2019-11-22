30f168dd954019479945cb643fd4de943db76a3c
Callum Leeming <callum.leeming@zuto.com>
2019-11-22 15:30:44 +0000

    Add errors.js

---
+++ PromisesAndErrorHandling/errors.js
@@ -0,0 +1,5 @@
+let example = something;
+
+console.log("hello world"); //this won't get hit, as there is an error above
+
+//Output: something is not defined at ​PromisesAndErrorHandling/errors.js:1:1​
