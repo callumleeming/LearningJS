12ddf13e41084c9b24e8d681f5ac03a49c3a8611
Callum Leeming <callum.leeming@zuto.com>
2019-11-25 12:50:30 +0000

    Update vehicle.js

--- ClassesAndModules/inheritance-example/vehicle.js
+++ ClassesAndModules/inheritance-example/vehicle.js
@@ -1,3 +1,6 @@
+// if this was using a transpiler, the syntax would look like the following:
+// export default class Vehicle {
+    
 module.exports = class Vehicle {
     constructor(vehicleType = "default") {
         this.vehicleType = vehicleType;
