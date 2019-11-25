12f3af08e3f1aaec34f552b666e22af065e6dadf
Callum Leeming <callum.leeming@zuto.com>
2019-11-25 12:49:04 +0000

    Update car.js

--- ClassesAndModules/inheritance-example/car.js
+++ ClassesAndModules/inheritance-example/car.js
@@ -1,5 +1,10 @@
 const Vehicle = require('./vehicle.js');
+//if this was using a transpiler, the syntax would look like the following:
+// import { Vehicle } from "./vehicle"
 
+
+//again, if this was using a transpiler, the syntax would look like the following:
+// export default class Car extends Vehicle {
 module.exports = class Car extends Vehicle {
     constructor() {
         super("car"); //Note: You must call super(); before accessing the 'this' keyword.
