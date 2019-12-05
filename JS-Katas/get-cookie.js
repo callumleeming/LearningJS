57a5b0cb31f1a5353247d555224bda4ddc13f067
Callum Leeming <callum.leeming@zuto.com>
2019-12-05 16:03:02 +0000

    Add get-cookie.js

---
+++ JS-Katas/get-cookie.js
@@ -0,0 +1,15 @@
+function getCookieValueByName(cookieName) {
+    var splitCookies = window.document.cookie.split("; ");
+    var cookies = {};
+
+    for (var index = 0; index < splitCookies.length; index++) {
+        var cookie = splitCookies[index].split("=");
+        var key = cookie[0];
+
+        cookies[key] = cookie[1];
+    }
+
+    return cookies[cookieName] || null;
+}
+
+console.log(getCookieValueByName("someCookie")); //someValue
